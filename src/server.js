18191d7b4640f50605129dd54a7c9eb02f8d4baf
codakawa <tynchtyk12264@gmail.com>
2024-01-27 01:27:00 +0600

    cors fix

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
+const cors = require('cors')
 const PORT = process.env.PORT || 3000;
 
 mongoose.connect(process.env.DATABASE_URL)
@@ -11,6 +12,7 @@ db.on("error", (error) => console.error(error))
 db.once('open', () => console.log('Connected to DB'))
 
 app.use(express.json({limit: "10mb"}))
+app.use(cors())
 
 const usersRouter = require(`./routes/users`)
 const animeListRouter = require(`./routes/animeList`)
